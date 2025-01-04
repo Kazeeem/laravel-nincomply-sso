ad377a5ef8a4e6a6e6af7457de2afac3791b08cf
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-01-04 18:38:18 +0100

    Minor fix

--- config/nincomply-sso.php
+++ config/nincomply-sso.php
@@ -3,7 +3,7 @@
 // config for Eximius/Nincomply
 
 return [
-    'client_id' => env('NINCOMPLY_CLIENT_ID', 'd03bf404-2ae8-4f24-a973-eeb2f3e60641'),
-    'client_secret' => env('NINCOMPLY_CLIENT_SECRET', 'sBFrrDXaRP8IQ3xIvMmxAY6C8CpVlnPsrTh0O9Ry7hO2WPoYMRcdkfOMMFC7'),
-    'redirect_uri' => env('NINCOMPLY_SSO_REDIRECT_URI', 'https://earncompass.ng'),
+    'client_id' => env('NINCOMPLY_CLIENT_ID'),
+    'client_secret' => env('NINCOMPLY_CLIENT_SECRET'),
+    'redirect_uri' => env('NINCOMPLY_SSO_REDIRECT_URI'),
 ];
